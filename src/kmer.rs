388fa38eea0f53296c7b2f035998b3216b493ac8
todd <tme17@pitt.edu>
2020-11-01 18:33:19 -0500

    Added identity calculation and began documenting

--- src/kmer.rs
+++ src/kmer.rs
@@ -1,8 +1,21 @@
 use std::collections::HashMap;
-use std::time::Instant;
+use std::cmp::max;
 
+/// A 2d reference into the query and subject sequences where a kmer match occurred.
+struct KmerMatchIndex {
+    /// The index within the query sequence the match occurred at.
+    query_index: usize,
+
+    /// The index within the subject sequence the match occurred at.
+    subject_index: usize,
+}
+
+/// A kmer match found between two sequences.
 struct MatchState {
+    /// The indices within the query and subject sequences the match was found at.
     index: (usize, usize),
+
+    /// The
     match_index: i32,
     score: i64,
 }
@@ -28,7 +41,7 @@ fn parse_kmers(genome: &str, kmer_size: usize) -> HashMap<&str, Vec<usize>> {
 fn cross_product(a: &Vec<usize>, b: &Vec<usize>, output: &mut Vec<(usize, usize)>) {
     for a_index in a {
         for b_index in b {
-            output.push((a_index.clone() + 1, b_index.clone() + 1));
+            output.push((a_index.clone(), b_index.clone()));
         }
     }
 }
@@ -61,7 +74,7 @@ fn distance_function(i: usize, j: usize) -> i64 {
     }
 }
 
-fn find_p_alignment(kmer_matches: &Vec<(usize, usize)>) {
+fn find_p_alignment(kmer_matches: &Vec<(usize, usize)>) -> Vec<(usize, usize)> {
     let mut backtrace_map: HashMap<usize, i32> = HashMap::new();
     let mut p_alignment: Vec<MatchState> = Vec::new();
 
@@ -108,9 +121,10 @@ fn find_p_alignment(kmer_matches: &Vec<(usize, usize)>) {
         }
     }
 
+    let mut pairs = Vec::new();
     let mut trace_p = p_alignment.first().unwrap();
     while backtrace_map.contains_key(&(trace_p.match_index as usize)) {
-        println!("{:?}", trace_p.index);
+        pairs.push(trace_p.index);
         let index_of_interest =
             kmer_matches[backtrace_map[&(trace_p.match_index as usize)] as usize];
         trace_p = p_alignment
@@ -119,18 +133,44 @@ fn find_p_alignment(kmer_matches: &Vec<(usize, usize)>) {
             .unwrap();
     }
 
-    println!("{:?}", trace_p.index);
+    pairs
 }
 
-pub fn find_alignment(first_sequence: &str, second_sequence: &str, kmer_size: usize) {
-    let now = Instant::now();
+fn calculate_distances(first_sequence_len: usize, second_sequence_len: usize, match_pairs: &Vec<(usize, usize)>) -> f64 {
+    let mut total_index: usize = 0;
+    let mut first_index: usize = 0;
+    let mut second_index: usize = 0;
+    let mut first_gap: usize = 0;
+    let mut second_gap: usize = 0;
+    for (query_pair_index, subject_pair_index) in match_pairs {
+        while total_index < max(query_pair_index.clone(), subject_pair_index.clone()) {
+            if first_index < query_pair_index.clone() {
+                first_index = first_index + 1;
+            } else {
+                first_gap = first_gap + 1;
+            }
+
+            if second_index < subject_pair_index.clone() {
+                second_index = second_index + 1;
+            } else {
+                second_gap = second_gap + 1;
+            }
+
+            total_index = total_index + 1;
+        }
+    }
+
+    let alignment_length = max(first_sequence_len + first_gap, second_sequence_len + second_gap) as f64;
+    1.0 - (match_pairs.len() as f64 / alignment_length)
+}
+
+pub fn find_alignment(first_sequence: &str, second_sequence: &str, kmer_size: usize) -> f64{
     let first_kmers = parse_kmers(&first_sequence, kmer_size);
     let second_kmers = parse_kmers(&second_sequence, kmer_size);
     let matches = calculate_and_sort_matches(&first_kmers, &second_kmers);
-    println!("4 step time : {}", now.elapsed().as_micros());
 
-    find_p_alignment(&matches);
-    println!("5 step time : {}", now.elapsed().as_micros());
+    let pairs = find_p_alignment(&matches);
+    calculate_distances(first_sequence.len(), second_sequence.len(), &pairs)
 }
 
 #[cfg(test)]
--- src/py.rs
+++ src/py.rs
@@ -5,7 +5,7 @@ use crate::kmer::find_alignment;
 #[pymodule]
 fn libkmer(_py: Python, m: &PyModule) -> PyResult<()> {
     #[pyfn(m, "find_alignment")]
-    fn find_alignment_py(_py: Python, first_sequence: &str, second_sequence: &str, kmer_size: usize) {
+    fn find_alignment_py(_py: Python, first_sequence: &str, second_sequence: &str, kmer_size: usize) ->  f64 {
         find_alignment(first_sequence, second_sequence, kmer_size)
     }
 
